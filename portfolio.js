3b4e6babc7c7b4d63e383b9b6959fa4495210c05
Maheer11 <117725925+Maheer11@users.noreply.github.com>
2025-06-25 18:36:03 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -4,3 +4,8 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.getElementById("arrow").onclick = function() {
+    document.getElementById("Projects").scrollIntoView({
+        behavior: "smooth"
+    })};
